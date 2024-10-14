5b08c17f901bfbffd0dfde9ce7abb90a5fdb65f7
Fahad Pathan <fahadpathan56@gmail.com>
2024-10-14 16:46:01 +0600

    add: learned relationship models (Django)
    1. admin panel modification
    2. relationship models

--- Django/myproject/myapp/admin.py
+++ Django/myproject/myapp/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import ChaVarity
+from .models import ChaVarity, ChaReview, Store, ChaCertificate
 
 # Register your models here.
-admin.site.register(ChaVarity) # This will add the model to the admin panel.
+
+class ChaReviewInline(admin.TabularInline):
+    model = ChaReview
+    extra = 2
+
+class ChaVarityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cha_type', 'price') # This will display the name, cha_type and price of the object in the admin panel.
+    inlines = [ChaReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address') # This will display the name and address of the object in the admin panel.
+    filter_horizontal = ('cha_variety',) # This will add a filter to the admin panel.
+
+class ChaCertificateAdmin(admin.ModelAdmin):
+    list_display = ('cha', 'certificate_number', 'issue_date')
+
+admin.site.register(ChaVarity, ChaVarityAdmin) # This will add the model to the admin panel.
+admin.site.register(ChaReview) # This will add the model to the admin panel.
+admin.site.register(Store, StoreAdmin) # This will add the model to the admin panel.
+admin.site.register(ChaCertificate, ChaCertificateAdmin) # This will add the model to the admin panel.
--- Django/myproject/myapp/models.py
+++ Django/myproject/myapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ChaVarity(models.Model):
@@ -19,3 +20,32 @@ class ChaVarity(models.Model):
 
     def __str__(self):
         return self.name # This will be the name of the object in the admin panel.
+
+# One to Many Relationship
+class ChaReview(models.Model):
+    cha = models.ForeignKey(ChaVarity, on_delete=models.CASCADE, related_name='reviews') # related_name is used to access the reviews of a particular cha.
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    review = models.TextField()
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username} review for {self.cha.name}" # This will be the name of the object in the admin panel.
+
+# Many to Many Relationship
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    cha_variety = models.ManyToManyField(ChaVarity, related_name='stores')
+
+    def __str__(self):
+        return self.name # This will be the name of the object in the admin panel.
+
+# One to One Relationship
+class ChaCertificate(models.Model):
+    cha = models.OneToOneField(ChaVarity, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issue_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.cha.name} certificate" # This will be the name of the object in the admin panel.
